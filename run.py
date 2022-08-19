8e920336e63f2c8d2130ef2dbe58b5dac46deae6
Farah M <farah.maria.rahman@gmail.com>
2022-08-19 21:24:07 +0000

    add credits to THE END lettering

--- run.py
+++ run.py
@@ -144,9 +144,19 @@ def Office():
     (_/   |   |_/|__/  |__/  |  |_/\_/|_/                            
                                        
     """)
-    
-    exit()
+# Script by Glenn Chappell 4/93
+# Includes ISO Latin-1
+# figlet release 2.1 -- 12 Aug 1994
+# Permission is hereby given to modify this font, as long as the
+# modifier's name is placed on a comment line.
 
+# Modified by Paul Burton <solution@earthlink.net> 
+# 12/96 to include new parameter
+# supported by FIGlet and FIGWin.  May also be slightly modified for better use
+# of new full-width/kern/smush alternatives, but default output is NOT changed.
+# from http://patorjk.com/
+
+    exit()
 
 # PART SEVEN: IF PLAYER BEAT TRUMP, TREATS!! AND TIME TO GO TO WORK.
 
