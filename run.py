de4b04bdc7392e2ebc11beb061675b46bc06dc5b
Farah M <farah.maria.rahman@gmail.com>
2022-08-19 00:06:36 +0000

    remove extra python file. incorrectly imported.

--- run.py
+++ run.py
@@ -2,7 +2,6 @@
 import random
 import gspread
 from google.oauth2.service_account import Credentials
-import stats
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -17,8 +16,66 @@ SHEET = GSPREAD_CLIENT.open('HJ_Scoresheet')
 
 scores_data = SHEET.worksheet('scores_data')
 data = scores_data.get_all_values()
+##############################################################
+# (pre-story set up of code)
 
 selectedCharacter = {}
+
+
+class Heroine:
+    """ Character for user to be through the game """
+    def __init__(self, name, fighting_spirit, self_esteem, calories_to_burn):
+        self.name = name
+        self.fighting_spirit = fighting_spirit
+        self.self_esteem = self_esteem
+        self.calories_to_burn = calories_to_burn
+
+    def printStats_and_start(self): 
+        # STARTS THE GAME AFTER PLAYER CHOOSES 'Y' (in PART 1)
+        """ Calls stats on player's choice of Heroine,""" 
+        """ & starts the story (with a bus fight!)"""
+        global selectedCharacter
+        print(f"You have selected {self.name}. These are their stats: ")
+        print(f"fighting spirit - {self.fighting_spirit}")
+        print(f"self esteem - {self.self_esteem}")
+        print(f"calories to burn - {self.calories_to_burn}")  
+        Bus_Fight()
+        """Heroine is taken to first part of the story"""
+
+# heroine stats:
+
+
+Demeter = Heroine("Demeter", 80, 80, 2800)
+
+Persephone = Heroine("Persephone", 90, 50, 1500)
+
+Athena = Heroine("Athena", 100, 90, 2000)
+
+Lilith = Heroine("Lilith", 75, 40, 3500)
+
+Roe = Heroine("Roe", 100, 95, 3000)
+
+
+# opponents who appear in battles:
+
+class Enemy:
+    """ Enemy who reduces player's power through the game """
+    def __init__(self, name, fighting_spirit, calories_to_burn):
+        self.name = name
+        self.fighting_spirit = fighting_spirit 
+        self.calories_to_burn = calories_to_burn
+
+# enemy stats
+
+
+Kavanaugh = Enemy("Kavanaugh", 40, 2500)
+
+Schlafly = Enemy("Schlafly", 60, 3000)
+
+Trump = Enemy("Trump", 100, 3500)
+
+###################################################################
+# STORY: start at the bottom of the page for part 1 and move up.
 
 
 # PART EIGHT of EIGHT: FIGHTING KAVANAUGH AT WORK. THE OFFICE IS UNSAFE!
