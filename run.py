ac92922d3b6aecaa231ffd3acd9283ee6050ce4e
Farah M <farah.maria.rahman@gmail.com>
2022-08-25 23:33:04 +0000

    comment out the match case codem as gitpod os too idiotic to deal with recent developments in python

--- run.py
+++ run.py
@@ -192,14 +192,29 @@ def play_again():
 
 
 def heroine_select():
-    """
-    Allows player to choose which heroine they play as. 
-    Selection takes them to start of the main story.
-    """
+    """ Allows player to choose which heroine they play as."""
     selection = input(
         "\n1. Demeter \n2. Persephone \n3. Athena \n4. Lilith \n5. Roe \
             \n")
+    if selection == "1":
+        return Heroine("Demeter", 80, 80, 2800)
+        """Heroine is taken to first part of the story"""
+    elif selection == "2":
+        return Heroine("Persephone", 90, 50, 1500)
+    elif selection == "3":
+        return Heroine("Athena", 100, 90, 2000)
+        """Heroine is taken to first part of the story"""
+    elif selection == "4":
+        return Heroine("Lilith", 75, 40, 3500)
+        """Heroine is taken to first part of the story"""
+    elif selection == "5":
+        return Heroine("Roe", 100, 95, 3000)
+    else:
+        slow_read(F"{Fore.RED}Error!")
+        slow_read("Only press 1, 2, 3, 4 or 5 and press enter")
+        heroine_select() 
 
+    """
     match selection:
         case "1":
             return Heroine("Demeter", 80, 80, 2800)
@@ -215,7 +230,7 @@ def heroine_select():
             slow_read(F"{Fore.RED}Error!")
             slow_read("Only press 1, 2, 3, 4 or 5 and press enter")
             heroine_select() 
-
+    """
 
 def score_end(heroine):
     """calculates and returns score at the end"""
