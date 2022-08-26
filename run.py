d305f940bd04090e3008d1b13c69bb1e478882d2
Farah M <farah.maria.rahman@gmail.com>
2022-08-26 20:00:17 +0000

    edit out matchcase version of heroine_select() again. Gitpod is rubbish.

--- run.py
+++ run.py
@@ -191,33 +191,35 @@ def play_again():
         play_again()
 
 
-"""def heroine_select():
-    #Allows player to choose which heroine they play as.
-    #They are then taken to start of adventure
-    selection = input(
-        "\n1. Demeter \n2. Persephone \n3. Athena \n4. Lilith \n5. Roe \
-            \n")
-    if selection == "1":
-        return Heroine("Demeter", 80, 80, 2800)
-    elif selection == "2":
-        return Heroine("Persephone", 90, 50, 1500)
-    elif selection == "3":
-        return Heroine("Athena", 100, 90, 2000)
-    elif selection == "4":
-        return Heroine("Lilith", 75, 40, 3500)
-    elif selection == "5":
-        return Heroine("Roe", 100, 95, 3000)
-    else:
-        slow_read(F"{Fore.RED}Error!")
-        slow_read("Only press 1, 2, 3, 4 or 5 and press enter")
-        heroine_select()""" 
-
-
 def heroine_select():
     """
     Allows player to choose which heroine they play as.
     They are then taken to start of adventure
     """
+    correct_input = False
+    while not correct_input:
+        selection = input(
+        "\n1. Demeter \n2. Persephone \n3. Athena \n4. Lilith \n5. Roe \
+            \n")
+        if selection == "1":
+            return Heroine("Demeter", 80, 80, 2800)
+        elif selection == "2":
+            return Heroine("Persephone", 90, 50, 1500)
+        elif selection == "3":
+            return Heroine("Athena", 100, 90, 2000)
+        elif selection == "4":
+            return Heroine("Lilith", 75, 40, 3500)
+        elif selection == "5":
+            return Heroine("Roe", 100, 95, 3000)
+        else:
+            slow_read(F"{Fore.RED}Error!")
+            slow_read("Only press 1, 2, 3, 4 or 5 and press enter")
+
+
+    """
+    def heroine_select():
+    # Allows player to choose which heroine they play as.
+    # They are then taken to start of adventure
     correct_input = False
     while not correct_input:
         selection = input(
@@ -237,6 +239,8 @@ def heroine_select():
             case _:
                 slow_read(F"{Fore.RED}Error!")
                 slow_read("Only press 1, 2, 3, 4 or 5 and press enter")
+        """
+    
     
 
 def score_end(heroine):
